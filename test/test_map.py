b908ddda5706d5aa636f104ee7b9a5584eb07314
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-02-04 19:30:43 +0100

    refactor: format with black

--- test/test_map.py
+++ test/test_map.py
@@ -16,43 +16,43 @@ def test_loading_bar(capfd):
     result = map(lambda x: x + 3, [1, 2, 3], loading_bar=True)
     assert result == [4, 5, 6]
     captured = capfd.readouterr()
-    assert captured.out == ''
-    assert '100%' in captured.err
-    assert '█' in captured.err
-    assert '3/3' in captured.err
-    assert 'it/s' in captured.err
+    assert captured.out == ""
+    assert "100%" in captured.err
+    assert "█" in captured.err
+    assert "3/3" in captured.err
+    assert "it/s" in captured.err
 
 
 def test_slow_loading(capfd):
     result = map(lambda x: time.sleep(1), [1, 2, 3], loading_bar=True)
     captured = capfd.readouterr()
-    assert captured.out == ''
-    assert '100%' in captured.err
-    assert '█' in captured.err
-    assert '3/3' in captured.err
-    assert '1.00s/it' in captured.err
+    assert captured.out == ""
+    assert "100%" in captured.err
+    assert "█" in captured.err
+    assert "3/3" in captured.err
+    assert "1.00s/it" in captured.err
 
 
 def test_loading_bar_arguments(capfd):
-    result = map(lambda x: x + 3, [1, 2, 3], loading_bar=True, desc='testoo')
+    result = map(lambda x: x + 3, [1, 2, 3], loading_bar=True, desc="testoo")
     assert result == [4, 5, 6]
     captured = capfd.readouterr()
-    assert captured.out == ''
-    assert 'testoo' in captured.err
+    assert captured.out == ""
+    assert "testoo" in captured.err
 
 
 def test_no_output(capfd):
     result = map(lambda x: x + 3, [1, 2, 3], loading_bar=False)
     assert result == [4, 5, 6]
     captured = capfd.readouterr()
-    assert captured.out == ''
-    assert captured.err == ''
+    assert captured.out == ""
+    assert captured.err == ""
 
 
 def test_zero_jobs():
     with pytest.raises(ValueError):
         map(lambda x: x + 3, [1, 2, 3], num_jobs=0)
-    
+
 
 def test_negative_jobs():
     with pytest.raises(ValueError):
